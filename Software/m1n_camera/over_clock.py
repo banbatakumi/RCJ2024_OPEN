11bc64cef4628ca566f90eec98984fa65752f8ba
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2025-04-20 21:57:42 +0900

    オーバークロックプログラム

---
+++ Software/m1n_camera/over_clock.py
@@ -0,0 +1,31 @@
+# オーバークロック用プログラム
+
+from Maix import freq  # 周波数読み出し
+
+# オーバークロックの設定：設定後自動再起動がかかる→その後コメントアウト
+# CPU[26-600] Max:800(MaixPy上では設定不可) default:400
+# pll1[26-1200] Max:1800(MaixPy上では設定不可) default:400
+# kpu_div[1-16] default:1 (kpu frequency = pll1/kpu_div)
+
+cpu_freq = 600 #!!!注!!!絶対に400以下にしない
+pll1 = 416
+kpu_div = 16
+
+freq.set(cpu=cpu_freq, pll1=pll1, kpu_div=kpu_div)
+
+import sensor, image, time, lcd
+
+sensor.reset()  # Reset and initialize the sensor. It will
+# run automatically, call sensor.run(0) to stop
+sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
+sensor.skip_frames(time=2000)  # Wait for settings take effect.
+clock = time.clock()  # Create a clock object to track the FPS.
+sensor.run(1)
+
+while True:
+    clock.tick()  # Update the FPS clock.
+    img = sensor.snapshot()  # Take a picture and return the image.
+    print(" FPS=%d" % clock.fps(), end="")  # FPSの出力：MaixPy上の値は実際の値の半分になる
+    print(" CPU_freq=%d" % freq.get_cpu(), end="")  # cpuの周波数：オーバークロックしてない場合はエラーになる
+    print(" KPU_freq=%d" % freq.get_kpu())  # kpuの周波数：オーバークロックしてない場合はエラーになる
